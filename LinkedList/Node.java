25ad09bb7f2a6ab8a06f3b2431719c8fc197946d
Shubham Tripathi <shubham.tripathi4@outlook.com>
2025-04-29 17:33:13 +0530

    Move Node to a separate .java file

---
+++ LinkedList/Node.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+class Node {
+    Node next;
+    Integer value;
+
+    Node(Integer value) {
+        next = null;
+        this.value = value;
+    }
+}
--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -24,12 +24,3 @@ class SingleLinkedList {
     }
 }
 
-class Node {
-    Node next;
-    Integer value;
-
-    Node(Integer value) {
-        next = null;
-        this.value = value;
-    }
-}
